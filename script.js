61f2265416382b3a2cb92c6ed6a80745643cef57
d-jey66 <jananashvilidachi@gmail.com>
2024-04-28 23:26:03 +0400

    portfolio :)

---
+++ script.js
@@ -0,0 +1,7 @@
+function showStats(){
+    const stats = document.querySelector('.stats');
+    const container = document.querySelector('.container');
+
+    stats.style = "display: flex;"
+    container.style = "height : 1100px;"
+}
